3604b8c2b369b89590989bd550f8e14ed745c7d1
Sainath <sainathsvm@gmail.com>
2021-09-04 15:29:49 +0530

    Added js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const angle1 = document.querySelector("#angle1")
+const angleValues = document.querySelectorAll(".angle-value");
+const btnCheck = document.querySelector(".btn-check");
+const output = document.querySelector("#output");
+
+function checkTraingle(){
+    var sum = 0;
+    for(let i=0;i<angleValues.length;i++){
+        sum = sum + Number(angleValues[i].value) ;
+    }
+    if( sum === 180){
+        output.innerText = "It's a Traingle";
+    }
+    else{
+        output.innerText = "It's not a Traingle";
+    }
+}
+
+
+btnCheck.addEventListener("click" ,function checkInput(){
+    if(angleValues.length === 3){
+        checkTraingle();
+    }
+    else{
+        output.innerText = "Fill all the fields";
+    }
+});
+
